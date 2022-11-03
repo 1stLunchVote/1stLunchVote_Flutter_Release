b7999eeb36e4d65e7d12a5e22f5a81be87e397d3
nerw173 <nerw173@gmail.com>
2022-11-03 18:28:53 +0900

    [ADD] 배경 도형 추가

--- lib/view/screen/profile_screen.dart
+++ lib/view/screen/profile_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:lunch_vote/view/widget/appbar_widget.dart';
+import 'package:lunch_vote/view/widget/utils/custom_clip_path.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +19,22 @@ class ProfileScreen extends StatelessWidget {
         isTitleCenter: true,
         context: context,
         trailingList: null,
-      )
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            ClipPath(
+              clipper: CustomClipPath(),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Theme.of(context).colorScheme.surfaceVariant,
+              ),
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }
