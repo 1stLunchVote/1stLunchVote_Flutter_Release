47bb3692955730d9bd65171a0a52c33e7faf3099
activedg <nerw173@gmail.com>
2022-11-13 20:08:46 +0900

    [ADD] 홈화면 컨테이너 버튼 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:lunch_vote/styles.dart';
+import 'package:lunch_vote/view/screen/home_screen.dart';
 import 'package:lunch_vote/view/screen/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'firebase_options.dart';
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
-void main() {
+void main() async {
   // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // runApp() 호출 전 Flutter SDK 초기화
   KakaoSdk.init(
-    nativeAppKey: '460927feb9d1b043b37bd492992f62b0',
-    javaScriptAppKey: 'c5e7ae17bbb1d4f127300c6c7038c8eb'
-  );
+      nativeAppKey: '460927feb9d1b043b37bd492992f62b0',
+      javaScriptAppKey: 'c5e7ae17bbb1d4f127300c6c7038c8eb');
 
   runApp(const MyApp());
 }
@@ -23,25 +29,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "1st Lunch Vote",
-      theme: ThemeData(
-          useMaterial3: true,
-          fontFamily: 'NanumSquareNeo',
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: mainColor,
-              brightness: Brightness.light),
-          scaffoldBackgroundColor: mainBackgroundColor
-      ),
+        debugShowCheckedModeBanner: false,
+        title: "1st Lunch Vote",
+        theme: ThemeData(
+            useMaterial3: true,
+            fontFamily: 'NanumSquareNeo',
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: mainColor, brightness: Brightness.light),
+            scaffoldBackgroundColor: mainBackgroundColor),
         darkTheme: ThemeData(
-          useMaterial3: true,
-          fontFamily: 'NanumSquareNeo',
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: mainColor,
-            brightness: Brightness.dark
-          )
-      ),
-      home: LoginScreen(),
-    );
+            useMaterial3: true,
+            fontFamily: 'NanumSquareNeo',
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: mainColor, brightness: Brightness.dark)),
+        home: LoginScreen());
   }
 }
--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lunch_vote/view/screen/profile_screen.dart';
 import 'package:lunch_vote/view/widget/appbar_widget.dart';
+import 'package:lunch_vote/view/widget/custom_clip_path.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,35 +11,119 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: BasicAppbar(
-        backVisible: false,
-        appbarTitle: '제 1회 점심메뉴 총선거',
-        isTitleCenter: true,
-        context: context,
-        trailingList: [
-          IconButton(
-              icon: Icon(Icons.account_circle,
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-              onPressed: (){
-                Navigator.of(context).push(
-                    MaterialPageRoute(
-                        builder: (context) => const ProfileScreen())
-                );
-              }
-            )
-        ],
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Image.network('https://my-hooni.s3.ap-northeast-2.amazonaws.com/%EA%B0%9C%EB%B0%9C%EC%9E%90+%EC%A7%A4.jpeg')
-            ],
-          )
-          ),
-
+        appBar: BasicAppbar(
+          backVisible: false,
+          appbarTitle: '제 1회 점심메뉴 총선거',
+          isTitleCenter: true,
+          context: context,
+          trailingList: [
+            IconButton(
+                icon: Icon(
+                  Icons.account_circle,
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ProfileScreen()));
+                })
+          ],
         ),
-      );
+        body: SafeArea(
+          child: SizedBox(
+            height: 800.h,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: ClipPath(
+                    clipper: CustomClipPath(),
+                    child: Container(
+                      width: 360.w,
+                      height: 400.h,
+                      color: Theme.of(context).colorScheme.surfaceVariant,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 200.h,
+                        width: 320.w,
+                        decoration: BoxDecoration(
+                            color:
+                                Theme.of(context).colorScheme.primaryContainer,
+                            borderRadius: BorderRadius.circular(16), //모서리를 둥글게
+                            border: Border.all(
+                                width: 1,
+                                color: Theme.of(context).colorScheme.outline)),
+                        child: Stack(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+                              child: Text('투표 방 만들기',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineSmall
+                                      ?.copyWith(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onPrimaryContainer)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 20, 20),
+                              child: Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Image.asset(
+                                      'assets/images/ic_lunch_vote.png')),
+                            ),
+                            Positioned.fill(
+                                child: Material(
+                              color: Colors.transparent,
+                              child: InkWell(
+                                borderRadius: BorderRadius.circular(16),
+                                onTap: () {},
+                              ),
+                            ))
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 200.h,
+                        width: 320.w,
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.background,
+                            borderRadius: BorderRadius.circular(16), //모서리를 둥글게
+                            border: Border.all(
+                                width: 1,
+                                color: Theme.of(context).colorScheme.outline)),
+                        child: Stack(children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+                            child: Text('템플릿 사전 설정하기',
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall),
+                          ),
+                          Positioned.fill(
+                              child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(16),
+                              onTap: () {},
+                            ),
+                          ))
+                        ]),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
