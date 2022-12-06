2004525050226b22cbd82b0e2dced24ce868e23e
이동건 <nerw173@gmail.com>
2022-12-06 15:12:34 +0900

    [ADD] 실시간 투표 연동 및 결과 화면 연동

---
+++ lib/controller/result_controller.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+
+import '../model/vote/final_result.dart';
+import '../repository/lunch_vote_service.dart';
+import '../view/widget/utils/shared_pref_manager.dart';
+
+class ResultController{
+  final dio = Dio();
+  late String groupId;
+  late LunchVoteService _lunchVoteService;
+  final SharedPrefManager _spfManager = SharedPrefManager();
+
+  ResultController(){
+    dio.options.headers["Content-Type"] = "application/json";
+  }
+
+  Future<FinalResult?> getFinalResult(String groupId) async{
+    this.groupId = groupId;
+    dio.options.headers["Authorization"] = await _spfManager.getUserToken();
+    dio.interceptors.add(LogInterceptor(responseBody: true, requestBody: true));
+    _lunchVoteService = LunchVoteService(dio);
+
+    var res = await _lunchVoteService.getFinalResult(groupId);
+    if (res.success){
+      return res.data;
+    }
+    return null;
+  }
+}
