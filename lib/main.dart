5059310303b91ddff046bf5faf847c53432a8bd5
이동건 <nerw173@gmail.com>
2023-01-03 21:25:43 +0900

    [Feature] Android Firebase Foreground 클릭 효과 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:ffi';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/scheduler.dart';
@@ -8,6 +9,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:lunch_vote/model/group_id_notifier.dart';
 import 'package:lunch_vote/styles.dart';
@@ -15,6 +18,7 @@ import 'package:lunch_vote/view/screen/home_screen.dart';
 import 'package:lunch_vote/view/screen/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lunch_vote/view/screen/profile_screen.dart';
 import 'package:lunch_vote/view/screen/vote/second_vote_screen.dart';
 import 'package:lunch_vote/view/widget/utils/shared_pref_manager.dart';
 import 'package:provider/provider.dart';
@@ -23,15 +27,12 @@ import 'firebase_options.dart';
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
 Future<void> main() async {
-  final SharedPrefManager _spfManager = SharedPrefManager();
   // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await initializeNotification();
 
   await dotenv.load(fileName: 'assets/config/.env');
   // runApp() 호출 전 Flutter SDK 초기화
@@ -40,13 +41,107 @@ Future<void> main() async {
   KakaoSdk.init(
     nativeAppKey: kakaoNativeAppKey
   );
+  runApp(const MyApp());
+}
 
-  await FirebaseMessaging.instance.getInitialMessage();
+Future initializeNotification() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-  runApp(ChangeNotifierProvider(
-    create: (context) => GroupIdNotifier(),
-    child: const MyApp(),
-  ));
+  final FirebaseMessaging messaging = FirebaseMessaging.instance;
+  print('FCM Token : ${await messaging.getToken()}');
+
+  // 로컬 알림 플러그인
+  var flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  // IOS 권한 체크
+  if (Platform.isIOS){
+    await requestPermission(messaging);
+  }
+  // Android channel 설정
+  var channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // name
+    description: 'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings(
+      '@mipmap/ic_launcher'
+  );
+  const DarwinInitializationSettings initializationSettingsIOS = DarwinInitializationSettings();
+  const InitializationSettings initializationSettings =
+  InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+  flutterLocalNotificationsPlugin.initialize(initializationSettings, onDidReceiveNotificationResponse: (response){
+    // Todo : 그룹 초대 화면으로 넘어가기
+    print("payload : ${response.payload}");
+    Get.to(() => const ProfileScreen());
+  });
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    if (message.notification != null) {
+      flutterLocalNotificationsPlugin.show(
+          message.hashCode,
+          message.notification?.title,
+          message.notification?.body,
+          NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channelDescription: channel.description,
+                icon: '@mipmap/ic_launcher',
+              ),
+              iOS: const DarwinNotificationDetails(
+                badgeNumber: 1,
+                subtitle: 'the subtitle',
+                sound: 'slow_spring_board.aiff',
+              )));
+    }
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+
+  await setupInteractedMessage();
+  // FCM Token Refresh
+  messaging.onTokenRefresh.listen((fcmToken) {
+    // TODO: If necessary send token to application server.
+
+  }).onError((err){
+    print(err);
+  });
+}
+
+Future requestPermission(FirebaseMessaging messaging) async{
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+}
+
+Future<void> setupInteractedMessage() async {
+  RemoteMessage? initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+
+  // 종료상태에서 클릭한 푸시 알림 메세지 핸들링
+  if (initialMessage != null) _handleMessage(initialMessage);
+
+  // 앱이 백그라운드 상태에서 푸시 알림 클릭 하여 열릴 경우 메세지 스트림을 통해 처리
+  FirebaseMessaging.onBackgroundMessage(_handleMessage);
+}
+
+Future<void> _handleMessage(RemoteMessage message) async {
+  // Todo : 임시로 프로필 화면으로 이동
+  Get.to(() => const ProfileScreen());
 }
 
 class MyApp extends StatefulWidget {
