f472d51eb577c85a12bb3c576c31633a19a86836
Kim Tae Woo <t.wooo@kakao.com>
2023-01-05 21:25:05 +0900

    [ADD] Add app login button

--- lib/view/screen/login_screen.dart
+++ lib/view/screen/login_screen.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lunch_vote/styles.dart';
 import 'package:lunch_vote/view/screen/home_screen.dart';
 import 'package:lunch_vote/controller/login_controller.dart';
 import 'package:lunch_vote/view/widget/utils/shared_pref_manager.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -58,35 +55,48 @@ class _LoginScreenState extends State<LoginScreen> {
                 "assets/images/lunch_vote_splash.png",
                 width: 150,
                 height: 150,
-              )
+              ),
             ),
           ),
-          Expanded(
-            child: Align(
-              alignment: FractionalOffset.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 100.0),
+          const Spacer(),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
                 child: MaterialButton(
-                onPressed: () async {
-                  _loginController.loginToken().then((value) {
-                    if (value != null) {
-                      _loginController.postUserToken(value).then((token) {
-                        if (token != null) {
-                          spfManager.setUserToken(token);
-                          print('User Token : $token');
-                          navigateToHome();
-                        }
-                      });
-                    }
-                  });
-                },
-                child: Image.asset(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => HomeScreen()));
+                  },
+                  child: Image.asset(
                     'assets/images/bg_kakao_login.png',
                     fit: BoxFit.fill,
                   ),
                 ),
               ),
-            )
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 80),
+                child: MaterialButton(
+                  onPressed: () async {
+                    _loginController.loginToken().then((value) {
+                      if (value != null) {
+                        _loginController.postUserToken(value).then((token) {
+                          if (token != null) {
+                            spfManager.setUserToken(token);
+                            print('User Token : $token');
+                            navigateToHome();
+                          }
+                        });
+                      }
+                    });
+                  },
+                  child: Image.asset(
+                    'assets/images/bg_default_login.png',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
